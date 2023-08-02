979ed14f44ec169f7934b405f29fd256ea21df1e
Kavindu95 <kavindueng95@gmail.com>
2023-08-02 20:09:48 +0530

    demo20 update

--- Demo20.java
+++ Demo20.java
@@ -1,29 +1,36 @@
 public class Demo20 {
     public static void main(String[] args) {
-        //(Simple|Complex)Data type -->String
-        int x=10;
+  
+        // (Primitive | Reference) Data Type -> String 
+
+        int x = 10;
         String myStr = x + "";
 
-        boolean myBoolean=false;
-        String myStr2 = myBoolean + "";
+        boolean myBool = false;
+        String myStr2 = myBool + "";
 
-        //String --> primitive data type
-
-        
-        
-        /*Wrwappers
-
-         * Byte.valueOf(String)->byte
-         * short.valueOf(String)->short
-         * String.charAt(index)->char
-         * Integer.valueOf(String)->int
-         * Long.valueOf(String)->long
-         * Float.valueOf(String)->float
-         * Double.valueOf(String)->double
+        // String -> Primitve Data Type
+        /* Wrappers
+        -------------
+         * Byte.valueOf(String) -> byte
+         * Short.valueOf(String) -> short
+         * "string".charAt(index) -> char
+         * Integer.valueOf(String) -> int
+         * Long.valueOf(String) -> long
+         * Float.valueOf(String) -> float
+         * Double.valueOf(String) -> double
          */
 
-        String myStr3 ="10";
-        byte myByte= Byte.valueOf(myStr3);
-        System.out.println(myByte);
+        String myStr3 = "10";
+        byte myByte = Byte.valueOf(myStr3);
+
+        String myStr4 = "10.25";
+        double myDouble = Double.valueOf(myStr4);
+
+        char myChar = "ABC".charAt(1);
+        System.out.println(myChar);
+
+        String myStr5 = "abc";
+        long myLong = Long.valueOf(myStr5);
     }
 }
